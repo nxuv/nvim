bbea3dec1d863804d38eeee638f9772895d045cb
al1-ce <al1-ce@null.net>
2025-12-11 16:24:15 +0300

    just.nvim hooks stuff

--- after/plugin/just.lua
+++ after/plugin/just.lua
@@ -1,13 +1,24 @@
 local noremap = require("remap").noremap
 
 if vim.fn.executable("just") and not vim.g.use_build_make then
-    if require("modules").try_setup("just", {
-        fidget_message_limit = 32,
-        play_sound = true,
-        open_qf_on_error = true,
-    }) then
+    if require("modules").can_load("just") then
+        local just = require("just").setup({
+            fidget_message_limit = 32,
+            play_sound = true,
+            open_qf_on_error = true,
+        })
         noremap("n", "<leader>bb", "<cmd>Just build<cr>", { desc = "Run build task" })
         noremap("n", "<leader>br", "<cmd>Just run<cr>"  , { desc = "Run run task"   })
+
+        if vim.fn.executable("aplay") ~= 1 then return end
+        local async = require("plenary.job")
+        local spath = vim.fs.dirname(vim.env.MYVIMRC)
+        just.add_callback_on_fail(function()
+            async:new( { command = "aplay", args = {string.format("%s/docs/build_success.wav", spath), "-q"} }) :start()
+        end)
+        just.add_callback_on_done(function()
+            async:new( { command = "aplay", args = {string.format("%s/docs/build_error.wav"  , spath), "-q"} }) :start()
+        end)
         return
     end
 
@@ -18,5 +29,4 @@ end
 
 noremap("n", "<leader>bb", function() vim.cmd("make") end)
 noremap("n", "<leader>br", function() vim.cmd("make run") end)
-
 
--- getdeps.sh
+++ getdeps.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-REQUIRED_PACKAGE_NAMES="git gcc tar curl fd ripgrep fzf nnn nodejs"
+REQUIRED_PACKAGE_NAMES="git tar curl fd ripgrep fzf nnn nodejs"
 # OPTIONAL_PACKAGE_NAMES="alsa-utils"
 NPM_PACKAGE_NAMES="tree-sitter-cli"
 # NPM_LSP_LIST="vim-language-server"
@@ -8,8 +8,9 @@ if [ -f /etc/os-release ]; then
     . /etc/os-release
     OS=$NAME
 else
-    echo "Missing /etc/os-release"
-    exit 0
+    # echo "Missing /etc/os-release"
+    # exit 0
+    OS="DOESNOTEXIST"
 fi
 
 # SOURCE: https://github.com/which-distro/os-release/tree/main
@@ -26,25 +27,37 @@ BASE_SUSE="|openSUSE|openSUSE Leap|openSUSE Tumbleweed|SLES"
 if [[ "$BASE_VOID" == *"|$OS"* ]]; then
     echo "Detected Void"
     echo "Installing dependencies from main repos"
-    sudo xbps-install -Sy $(REQUIRED_PACKAGE_NAMES)
+    sudo xbps-install -Sy gcc
+    sudo xbps-install -Sy $REQUIRED_PACKAGE_NAMES
     echo "Installing dependencies from npm"
-    npm install -g $(NPM_PACKAGE_NAMES)
+    npm install -g $NPM_PACKAGE_NAMES
 fi
 
 if [[ "$BASE_DEBIAN" == *"|$OS"* ]]; then
     echo "Detected Debian"
     echo "Installing dependencies from main repos"
     sudo apt update
-    sudo apt install $(REQUIRED_PACKAGE_NAMES)
+    sudo apt install gcc
+    sudo apt install $REQUIRED_PACKAGE_NAMES
     echo "Installing dependencies from npm"
-    npm install -g $(NPM_PACKAGE_NAMES)
+    npm install -g $NPM_PACKAGE_NAMES
 fi
 
 if [[ "$BASE_ARCH" == *"|$OS"* ]]; then
     echo "Detected Arch"
     echo "Installing dependencies from main repos"
-    sudo pacman -Sy $(REQUIRED_PACKAGE_NAMES)
+    sudo pacman -Sy gcc
+    sudo pacman -S $REQUIRED_PACKAGE_NAMES
     echo "Installing dependencies from npm"
-    npm install -g $(NPM_PACKAGE_NAMES)
+    npm install -g $NPM_PACKAGE_NAMES
+fi
+
+if [[ "$(uname -o)" == "Android" ]]; then
+    echo "Detected Termux"
+    echo "Installing dependencies from main repos"
+    apt install termux-api
+    apt install $REQUIRED_PACKAGE_NAMES
+    echo "Installing dependencies from npm"
+    npm install -g $NPM_PACKAGE_NAMES
 fi
 
